98490b8b7648ae4d2a739dbf51dfa53057f7a052
KerinAO <90246117+KerinAO@users.noreply.github.com>
2022-02-01 12:57:54 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trokut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kateta1;
+            int kateta2;
+            int hipotenuza;
+
+            Console.WriteLine("Unesi vrijednosti trokuta: ");
+            kateta1 = Convert.ToInt32(Console.ReadLine());
+            kateta2 = Convert.ToInt32(Console.ReadLine());
+            hipotenuza = Convert.ToInt32(Console.ReadLine());
+
+            double x = Math.Pow(hipotenuza, 2);
+            double y = Math.Pow(kateta1, 2);
+            double z = Math.Pow(kateta2, 2);
+
+            if (Convert.ToBoolean(x=y+z))
+            {
+                Console.WriteLine("Trokut je pravokutan");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
